dc882251a8db0ec50b09714e17aa74967bccd00f
wildflower23 <161136642+wildflower23@users.noreply.github.com>
2024-06-27 11:17:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './Counter';
+import store from './store';
+
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Counter = ({ count, increment, decrement }) => {
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { count: state.count };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => dispatch({ type: 'INCREMENT' }),
+    decrement: () => dispatch({ type: 'DECREMENT' })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
